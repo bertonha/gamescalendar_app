834311e86b489cab0d45e6e357746535a1d8f4f7
Christofer Bertonha <christoferbertonha@gmail.com>
2025-02-23 20:29:14 +0100

    use proxy server

--- lib/api.dart
+++ lib/api.dart
@@ -2,45 +2,25 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'game.dart';
 
-const clientId = String.fromEnvironment("CLIENT_ID");
-const token = String.fromEnvironment("TOKEN");
-
 Future<List<GameRelease>> fetchReleaseGames(
   List<Platform> platforms,
   DateTime start,
   DateTime end,
 ) async {
-  if (clientId.isEmpty) {
-    throw Exception("CLIENT_ID not set");
-  }
-  if (token.isEmpty) {
-    throw Exception("TOKEN not set");
-  }
-
   if (platforms.isEmpty) {
     return [];
   }
 
-  var epochStart = start.millisecondsSinceEpoch ~/ 1000;
-  var epochEnd = end.millisecondsSinceEpoch ~/ 1000;
-
-  var formatedPlatforms = platforms
-      .map((e) => e.id.toString())
-      .toList()
-      .join(",");
+  var formatedPlatforms = platforms.map((e) => e.name).toList();
 
   final response = await http.post(
-    Uri.parse('https://api.igdb.com/v4/release_dates'),
-    headers: {"Client-ID": clientId, "Authorization": "Bearer $token"},
-    body: """
-        fields game.name,game.cover.url,platform,date;
-        where
-        date > $epochStart
-        & date < $epochEnd
-        & platform = ($formatedPlatforms);
-        sort date asc;
-        limit 200;
-    """,
+    Uri.parse('https://gamescalendar-726749962824.europe-southwest1.run.app'),
+    headers: {'Content-Type': 'application/json; charset=UTF-8'},
+    body: jsonEncode({
+      "start_date": start.toIso8601String(),
+      "end_date": end.toIso8601String(),
+      "platforms": formatedPlatforms,
+    }),
   );
 
   if (response.statusCode == 200) {
