d763e462e341d7a63d9449977df8c2dcc70f74e6
Christofer Bertonha <christoferbertonha@gmail.com>
2025-02-24 01:02:14 +0100

    use utc datetime

--- lib/api.dart
+++ lib/api.dart
@@ -17,8 +17,8 @@ Future<List<GameRelease>> fetchReleaseGames(
     Uri.parse('https://gamescalendar-726749962824.europe-southwest1.run.app'),
     headers: {'Content-Type': 'application/json; charset=UTF-8'},
     body: jsonEncode({
-      "start_date": start.toIso8601String(),
-      "end_date": end.toIso8601String(),
+      "start_date": start.toUtc().toIso8601String(),
+      "end_date": end.toUtc().toIso8601String(),
       "platforms": formatedPlatforms,
     }),
   );
